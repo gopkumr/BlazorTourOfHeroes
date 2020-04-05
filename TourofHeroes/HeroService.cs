fcbda6eb52805706c03335424bc31cef7f0054ca
Gopakumar R <Gopakumar.R@live.com>
2020-04-05 20:13:06 +1000

    Added search functionality

--- TourofHeroes/HeroService.cs
+++ TourofHeroes/HeroService.cs
@@ -8,6 +8,14 @@ namespace TourofHeroes
     public class HeroService
     {
         private readonly MessageService messageService;
+        private List<Hero> MasterListOfHeros = new List<Hero>()
+        {
+            new Hero() { Id = 1, Name = "Test1" },
+            new Hero() { Id = 2, Name = "Test2" },
+            new Hero() { Id = 3, Name = "Test3" },
+            new Hero() { Id = 4, Name = "Test4" },
+            new Hero() { Id = 5, Name = "Test5" }
+        };
 
         public HeroService(MessageService _messageService)
         {
@@ -17,15 +25,22 @@ namespace TourofHeroes
         public List<Hero> GetHeroes()
         {
             //messageService.Add("HeroService: fetched heroes");
+            return MasterListOfHeros;
+        }
 
-            return new List<Hero>()
-                {
-                    new Hero(){ Id=1, Name="Test1"},
-                    new Hero(){ Id=2, Name="Test2"},
-                    new Hero(){ Id=3, Name="Test3"},
-                    new Hero(){ Id=4, Name="Test4"},
-                    new Hero(){ Id=5, Name="Test5"}
-                };
+        public List<Hero> SearchHeros(string searchTerm)
+        {
+            if (string.IsNullOrEmpty(searchTerm)) return new List<Hero>();
+            //messageService.Add("HeroService: fetched heroes");
+            return MasterListOfHeros.Where(q => q.Name.ToLower().StartsWith(searchTerm.ToLower())).ToList();
+        }
+
+        public List<Hero> AddNewHero(Hero newHero)
+        {
+            //messageService.Add("HeroService: fetched heroes");
+
+            MasterListOfHeros.Add(newHero);
+            return MasterListOfHeros;
         }
     }
 }
