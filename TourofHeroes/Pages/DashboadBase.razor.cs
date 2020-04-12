70f99e1084a9403cdbb3df092ca4dc12e8859c5a
Gopakumar R <Gopakumar.R@live.com>
2020-04-12 11:38:06 +1000

    Added Separate code file for Dashboard

---
+++ TourofHeroes/Pages/DashboadBase.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourofHeroes.Pages
+{
+    public class DashboadBase:ComponentBase
+    {
+        [Inject]
+        public HeroService _heroService
+        {
+            get;set;
+        }
+        protected List<Hero> topFive;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var _heroesList = await Task.FromResult(_heroService.GetHeroes());
+            topFive = _heroesList.OrderByDescending(q => q.Id).Take(4).ToList();
+        }
+    }
+}
